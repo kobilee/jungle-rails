f0c4f12ec640a6d841e78972de98f9ff810277bf
Kobi Lee <kobiatlaslee@gmail.com>
2019-05-17 19:40:48 +0000

    step 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,12 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :reviews
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
+
 
 end
